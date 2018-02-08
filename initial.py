3265aa26d46d1e1fcd57c03880d7ea3664ec1acf
Jonathan <jonathan_plata@ymail.com>
2018-02-07 21:08:43 -0800

    weapon setup

--- initial.py
+++ initial.py
@@ -1,20 +1,28 @@
 import inspect 
 
 class Fighter:
-	name = "Bob"
-	health = 40
-	strength = 10
-	defense = 4
-	agility = 5
-	wisdom = 1.1
-	
+
+	def __init__(self):	
+		self.name = "Bob"
+		self.level = 1
+		self.health = 40
+		self.strength = 10
+		self.defense = 4
+		self.agility = 5
+		self.wisdom = 1.2
+		self.weapon = 'Typewriter'
+
 	def __setName__(self, n):
 		self.name = n
 
+	def __setWeapon__(self, w):
+		self.weapon = w
+
 	def __levelUp__(self):
+		self.level += 1
 		self.health += 2 * self.wisdom
-		self.strength += 3 * self.wisdom
-		self.defense =+ 3 * self.wisdom
+		self.strength += 2.2 * self.wisdom
+		self.defense += 1.5 * self.wisdom
 
 	def __showStats__(self):
 		print('\n======  {name} ======'.format(name = self.name))
@@ -23,17 +31,31 @@ class Fighter:
 		print(stuff)
 
 		for attr in stuff:
-			print(attr)
+			a = attr + " a"
+			print(a)
 
 		# print('Health: {hp}'.format(hp = self.health))
 		# print('Strength: {str}'.format(str = self.str))
 		# print('Health: {hp}'.format(hp = self.health))
 		
 
-testUser = Fighter()
+current_user = Fighter()
 
-testUser.__showStats__()
+print (current_user.__dict__)
 
-# testUser.__levelUp()
+n = str(input('\nWhat is your name?  '))
 
-# testUser.__showStats()
+current_user.__setName__(n)
+
+print('''\nAlright {n}, you have to escape the haunted forest
+Who left you there? Who knows
+
+  '''.format(n = current_user.name))
+
+print("Just some some beasts and a demon or two...\n\nDon't worry you got this")
+
+print("So what kind of weapon do you want?\n")
+
+w = str(input('be modest  '))
+
+# print(current_user.__dict__)
